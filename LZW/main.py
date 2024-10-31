d4739b7b749ecbef9f06ad7d78f4649708172559
Sobhi Mohamed <sobhielsayd48@gmail.com>
2024-10-31 16:45:59 +0300

    complete decompressed function in LZW

--- LZW/main.py
+++ LZW/main.py
@@ -36,5 +36,53 @@ def match(data,listOfCode,i):
     return len(string_match)
 
 
-print(compressed("ABAABABBAABAABAAAABABBBBBBBB"))
+# print(compressed("ABAABABBAABAABAAAABABBBBBBBB"))
+#                   ABAABABBAABAABAAAABABBBBBBBB
+# [65, 66, 65, 128, 128, 129, 131, 134, 130, 129, 66, 138, 139, 138]
 
+def decompressed(compressedData):
+    listOfCode={}
+    data=""
+    i=0
+    keyOfListCode=128
+    index=0
+    while i<len(compressedData):
+        index=len(data)
+        if compressedData[i]<128:
+            data+=chr(compressedData[i])
+            if i!=0 and compressedData[i-1]<128:
+                if data[index-1:1+index]not in listOfCode.values()  :
+                    listOfCode[keyOfListCode] = data[index-1:1+index]
+                    keyOfListCode+=1
+            elif i!=0:
+                if data[index-len(listOfCode[compressedData[i-1]]):1+index]not in listOfCode.values()  :
+                    listOfCode[keyOfListCode] = data[index-len(listOfCode[compressedData[i-1]]):1+index]
+                    keyOfListCode+=1
+
+        elif compressedData[i] not in listOfCode.keys():
+            if compressedData[i-1]<128:
+                data+=chr(compressedData[i-1])+chr(compressedData[i-1])
+                if data[index - 1:1 + index] not in listOfCode.values() :
+                    listOfCode[keyOfListCode] = data[index - 1:index + 1]
+                    keyOfListCode+=1
+            else:
+                data+=listOfCode[compressedData[i-1]]+listOfCode[compressedData[i-1]][0]
+                if data[index - (len(listOfCode[compressedData[i-1]])):index + 1] not in listOfCode.values() :
+
+                    listOfCode[keyOfListCode] = data[index - (len(listOfCode[compressedData[i-1]])):index + 1]
+                    keyOfListCode+=1
+        else:
+            data+=listOfCode[compressedData[i]]
+            if compressedData[i-1]<128:
+                if data[index-1:index+1] not in listOfCode.values()  :
+                    listOfCode[keyOfListCode]=data[index-1:index+1]
+                    keyOfListCode += 1
+            else:
+                if data[index-len(listOfCode[compressedData[i-1]]):index+1] not in listOfCode.values() :
+                    listOfCode[keyOfListCode]=data[index-(len(listOfCode[compressedData[i-1]])):index+1]
+                    keyOfListCode += 1
+
+        i+=1
+    return data
+# print(compressed("sobhi mohamed sobhi"))
+print(decompressed([115, 111, 98, 104, 105, 32, 109, 111, 104, 97, 109, 101, 100, 32, 128, 130, 105]))
